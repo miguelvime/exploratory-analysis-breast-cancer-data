cd39d80c0a6469a1718158d4f7f14483ba400cd7
miguelvime <miguelvicentemeso@gmail.com>
2026-01-20 23:51:16 +0100

    Inlcluyo script R inicial

---
+++ 0JCP001104_UA1_AA1.R
@@ -0,0 +1,37 @@
+#### Actividad Unidad 1, Modulo 4
+## Uso de las herramientas R y RStudio para la investigacion biomedica 
+# Nombre y apellidos: Miguel Ángel Vicente Mesonero
+# Leer y rellenar el documento de actividad correspondiente
+
+####################################################
+# Rellenar los comandos necesarios en este archivo para las preguntas propuestas en el documento de actividad.
+
+# Cargar librerías necesarias
+library(tidyverse)
+library(data.table)
+library(dplyr)
+# 1. Lectura del archivo nationwidechildrens.org_clinical_patient_brca.txt en data frame 'breast_data'
+breast_data<-fread("nationwidechildrens.org_clinical_patient_brca.txt")
+
+## Limpieza: las primeras dos filas no corresponden a la información de los pacientes.
+breast_data <- breast_data[-c(1,2),]
+
+# 2 y 3. Centr?donos en la variable ajcc_pathologic_tumor_stage, crear un nuevo data frame (breast_data_stage) que contenga como variables la edad de diagn?stico
+#, el estadio del tumor y una variable que agrupe los diferencas estadios en cinco.
+
+breast_data_stage <- select(breast_data$age_at_diagnosis,breast_data$ajcc_pathologic_tumor_stage) %>% 
+  mutate(breast_data_stage, stage
+
+# 4. An?lisis exploratorio de diagrama de cajas (boxplot)
+
+
+# 5. Estudio de la edad media de diagn?stico para cada estadio de la enfermedad
+
+
+# 6. Test de comparaci?n de medias muestrales independientes para el estudio de diferencias en la edad de diagn?stico cuando se compara el estadio I y estadio III.
+
+
+# 7. An?lisis de supervivencia
+
+
+
